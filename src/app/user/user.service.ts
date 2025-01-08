a300e6087064d3da24ad633c21890479cfe336af
argranados <argranados@yahoo.com>
2025-01-08 11:32:47 -0600

    add serice test

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  users = [
+    {id: 1, anme: "John Doe" },
+    {id: 2, anme: "maria Doe" }
+  ]
+
+  constructor() { }
+
+  getusers(){
+    return of(this.users)
+  }
+}
